afbb96b5e581e6113a18c148e028ff59b2db7939
Adithya-ScriptKindle <adisursad@gmail.com>
2024-04-11 10:48:38 -0400

    all changes

---
+++ reference/os_demo.js
@@ -0,0 +1,15 @@
+const os =require('os');
+//Platform 
+console.log(os.platform());
+//CPU Architecture
+console.log(os.arch());
+//CPu Core information
+console.log(os.cpus());
+// Free memory
+console.log(os.freemem()); 
+//total memeory
+console.log(os.totalmem());
+//home directory
+console.log(os.homedir());
+//uptime 
+console.log(os.uptime()); 
